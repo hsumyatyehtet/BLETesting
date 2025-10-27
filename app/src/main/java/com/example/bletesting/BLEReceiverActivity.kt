d6cd72c39d48ef4bc405ba722cd0aecccb8ceb38
hsumyatyehtet <hsumyatyehtet@gmail.com>
2025-10-27 16:22:58 +0630

    Initial commit

---
+++ app/src/main/java/com/example/bletesting/BLEReceiverActivity.kt
@@ -0,0 +1,99 @@
+package com.example.bletesting
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.os.ParcelUuid
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresPermission
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.nio.charset.StandardCharsets
+
+class BLEReceiverActivity: AppCompatActivity() {
+
+    private lateinit var bluetoothLeScanner: BluetoothLeScanner
+    private lateinit var txtStatus: TextView
+
+    private val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { result ->
+        if (result.values.all { it }) startScan()
+        else Toast.makeText(this, "Permissions denied", Toast.LENGTH_SHORT).show()
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ble_receiver)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        txtStatus = findViewById(R.id.tvReceiver)
+
+        requestPermissions()
+
+
+    }
+
+    private fun requestPermissions() {
+        val permissions = mutableListOf(
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            permissions.add(Manifest.permission.BLUETOOTH_SCAN)
+        }
+        permissionLauncher.launch(permissions.toTypedArray())
+    }
+
+    private fun startScan() {
+
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+
+        val filter = ScanFilter.Builder()
+            .setServiceUuid(ParcelUuid(SharedConstants.SERVICE_UUID))
+            .build()
+
+        val settings = ScanSettings.Builder()
+            .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+            .build()
+
+        bluetoothLeScanner.startScan(listOf(filter), settings, scanCallback)
+        txtStatus.text = "Scanning..."
+    }
+
+    private val scanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            val serviceData = result.scanRecord?.getServiceData(ParcelUuid(SharedConstants.SERVICE_UUID))
+            serviceData?.let {
+                val message = String(serviceData, StandardCharsets.UTF_8)
+                txtStatus.text = "Received: $message"
+                Log.d("BLE_RECEIVER", "Received Data: $message")
+            }
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/bletesting/SharedConstants.kt
@@ -0,0 +1,10 @@
+package com.example.bletesting
+
+import java.util.UUID
+
+object SharedConstants {
+
+    val SERVICE_UUID: UUID = UUID.fromString("0000abcd-0000-1000-8000-00805f9b34fb")
+    const val ADVERTISING_MESSAGE = "Hello Receiver"
+
+}
